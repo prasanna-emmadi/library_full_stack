5391a5ea33210c27869c6982c942a5caf2677767
prasanna-emmadi <48420408+prasanna-emmadi@users.noreply.github.com>
2023-11-20 23:41:19 +0200

    Removing unused useState in Logout.js

--- library_ui/src/components/Logout.js
+++ library_ui/src/components/Logout.js
@@ -1,5 +1,4 @@
 import { Button } from "@chakra-ui/react";
-import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { useAuthContext } from "../contexts/authContext";
 
