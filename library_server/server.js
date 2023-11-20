2dac5153024fa34e38aa178458d22d0390c6b038
prasanna-emmadi <48420408+prasanna-emmadi@users.noreply.github.com>
2023-11-20 22:30:09 +0200

    Fixing databse name

--- library_server/server.js
+++ library_server/server.js
@@ -26,7 +26,7 @@ app.use((req, res, next) => {
 
 const connectMongoose = async () => {
   try {
-    await mongoose.connect("mongodb://localhost:27017/EasyBuyDB");
+    await mongoose.connect("mongodb://localhost:27017/LibraryDB");
     console.log("connected to mongoose");
   } catch (e) {
     console.error(e);
